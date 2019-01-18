34ceb635e19e9321b648cd78f62ef765b977a334
Richard Walker <digitalsadhu@gmail.com>
2019-01-18 13:35:31 +0100

    add summary class and tests

---
+++ test/summary.js
@@ -0,0 +1,242 @@
+'use strict';
+
+const tap = require('tap');
+const lolex = require('lolex');
+const Summary = require('../lib/summary');
+
+tap.test('summary() - creating a basic summary without options throws', t => {
+    t.throws(() => new Summary());
+    t.end();
+});
+
+tap.test('summary().observe() - calling observe with no argument throws', t => {
+    const summary = new Summary({
+        name: 'valid_name',
+        description: 'Valid description',
+    });
+    t.throws(() => summary.observe());
+    t.end();
+});
+
+tap.test('summary() - creating a basic summary with minimal arguments', t => {
+    const summary = new Summary({
+        name: 'valid_name',
+        description: 'Valid description',
+    });
+
+    summary.on('metric', metric => {
+        t.equal(metric.name, 'valid_name');
+        t.equal(metric.description, 'Valid description');
+        t.equal(metric.type, 7);
+        t.equal(metric.value, 1);
+        t.same(metric.labels, []);
+        t.same(metric.meta, {});
+        t.end();
+    });
+
+    summary.observe(1);
+});
+
+tap.test(
+    'summary() - creating a summary with labels and then not populating them',
+    t => {
+        const summary = new Summary({
+            name: 'valid_name',
+            description: 'Valid description',
+            labels: { first: undefined, second: undefined, third: undefined },
+        });
+
+        summary.on('metric', metric => {
+            t.equal(metric.name, 'valid_name');
+            t.equal(metric.description, 'Valid description');
+            t.equal(metric.type, 7);
+            t.equal(metric.value, 1);
+            t.same(metric.labels, [
+                { name: 'first', value: undefined },
+                { name: 'second', value: undefined },
+                { name: 'third', value: undefined },
+            ]);
+            t.same(metric.meta, {});
+            t.end();
+        });
+
+        summary.observe(1);
+    },
+);
+
+tap.test(
+    'summary() - creating a summary with labels and then populating them',
+    t => {
+        const summary = new Summary({
+            name: 'valid_name',
+            description: 'Valid description',
+            labels: { first: undefined, second: undefined, third: undefined },
+        });
+
+        summary.on('metric', metric => {
+            t.equal(metric.name, 'valid_name');
+            t.equal(metric.description, 'Valid description');
+            t.equal(metric.type, 7);
+            t.equal(metric.value, 1);
+            t.same(metric.labels, [
+                { name: 'first', value: 'this is first' },
+                { name: 'second', value: 'this is second' },
+                { name: 'third', value: 'this is third' },
+            ]);
+            t.same(metric.meta, {});
+            t.end();
+        });
+
+        summary.observe(1, {
+            labels: {
+                first: 'this is first',
+                second: 'this is second',
+                third: 'this is third',
+            },
+        });
+    },
+);
+
+tap.test(
+    'summary() - creating a summary without labels and then populating them',
+    t => {
+        const summary = new Summary({
+            name: 'valid_name',
+            description: 'Valid description',
+        });
+
+        summary.on('metric', metric => {
+            t.equal(metric.name, 'valid_name');
+            t.equal(metric.description, 'Valid description');
+            t.equal(metric.type, 7);
+            t.equal(metric.value, 1);
+            t.same(metric.labels, [
+                { name: 'first', value: 'this is first' },
+                { name: 'second', value: 'this is second' },
+                { name: 'third', value: 'this is third' },
+            ]);
+            t.same(metric.meta, {});
+            t.end();
+        });
+
+        summary.observe(1, {
+            labels: {
+                first: 'this is first',
+                second: 'this is second',
+                third: 'this is third',
+            },
+        });
+    },
+);
+
+tap.test(
+    'summary() - creating a summary with some labels and then populating them others (merge)',
+    t => {
+        const summary = new Summary({
+            name: 'valid_name',
+            description: 'Valid description',
+            labels: { first: 'this is first' },
+        });
+
+        summary.on('metric', metric => {
+            t.equal(metric.name, 'valid_name');
+            t.equal(metric.description, 'Valid description');
+            t.equal(metric.type, 7);
+            t.equal(metric.value, 1);
+            t.same(metric.labels, [
+                { name: 'first', value: 'this is first' },
+                { name: 'second', value: 'this is second' },
+                { name: 'third', value: 'this is third' },
+            ]);
+            t.same(metric.meta, {});
+            t.end();
+        });
+
+        summary.observe(1, {
+            labels: {
+                second: 'this is second',
+                third: 'this is third',
+            },
+        });
+    },
+);
+
+tap.test('summary() - using timer to time a number of seconds', t => {
+    const clock = lolex.install();
+
+    const summary = new Summary({
+        name: 'valid_name',
+        description: 'Valid description',
+        labels: { first: 'this is first' },
+    });
+
+    summary.on('metric', metric => {
+        t.equal(metric.name, 'valid_name');
+        t.equal(metric.description, 'Valid description');
+        t.equal(metric.type, 7);
+        t.equal(metric.value, 0.231);
+        t.same(metric.labels, [
+            { name: 'first', value: 'this is first' },
+            { name: 'second', value: 'this is second' },
+            { name: 'third', value: 'this is third' },
+        ]);
+        t.same(metric.meta, {});
+        t.end();
+    });
+
+    const end = summary.timer({
+        labels: {
+            second: 'this is second',
+            third: 'this is third',
+        },
+    });
+
+    clock.tick(231);
+
+    end();
+
+    clock.uninstall();
+});
+
+tap.test(
+    'summary() - using timer to time a number of seconds, label merge',
+    t => {
+        const clock = lolex.install();
+
+        const summary = new Summary({
+            name: 'valid_name',
+            description: 'Valid description',
+            labels: { first: 'this is first' },
+        });
+
+        summary.on('metric', metric => {
+            t.equal(metric.name, 'valid_name');
+            t.equal(metric.description, 'Valid description');
+            t.equal(metric.type, 7);
+            t.equal(metric.value, 0.231);
+            t.same(metric.labels, [
+                { name: 'first', value: 'this is first' },
+                { name: 'second', value: 'this is second' },
+                { name: 'third', value: 'this is third' },
+            ]);
+            t.same(metric.meta, {});
+            t.end();
+        });
+
+        const end = summary.timer({
+            labels: {
+                second: 'this is second',
+            },
+        });
+
+        clock.tick(231);
+
+        end({
+            labels: {
+                third: 'this is third',
+            },
+        });
+
+        clock.uninstall();
+    },
+);
