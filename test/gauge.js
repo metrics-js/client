502f90664a449f6923ed6b89ec45ef7681b2f947
Richard Walker <digitalsadhu@gmail.com>
2019-01-18 12:39:21 +0100

    add additional tests to gauge

--- test/gauge.js
+++ test/gauge.js
@@ -95,3 +95,67 @@ tap.test(
         });
     },
 );
+
+tap.test(
+    'gauge() - creating a gauge without labels and then populating them',
+    t => {
+        const gauge = new Gauge({
+            name: 'valid_name',
+            description: 'Valid description',
+        });
+
+        gauge.on('metric', metric => {
+            t.equal(metric.name, 'valid_name');
+            t.equal(metric.description, 'Valid description');
+            t.equal(metric.type, 1);
+            t.equal(metric.value, 1);
+            t.same(metric.labels, [
+                { name: 'first', value: 'this is first' },
+                { name: 'second', value: 'this is second' },
+                { name: 'third', value: 'this is third' },
+            ]);
+            t.same(metric.meta, {});
+            t.end();
+        });
+
+        gauge.set(1, {
+            labels: {
+                first: 'this is first',
+                second: 'this is second',
+                third: 'this is third',
+            },
+        });
+    },
+);
+
+tap.test(
+    'gauge() - creating a gauge with some labels and then populating them others (merge)',
+    t => {
+        const gauge = new Gauge({
+            name: 'valid_name',
+            description: 'Valid description',
+            labels: { first: 'this is first' },
+        });
+
+        gauge.on('metric', metric => {
+            t.equal(metric.name, 'valid_name');
+            t.equal(metric.description, 'Valid description');
+            t.equal(metric.type, 1);
+            t.equal(metric.value, 1);
+            t.same(metric.labels, [
+                { name: 'first', value: 'this is first' },
+                { name: 'second', value: 'this is second' },
+                { name: 'third', value: 'this is third' },
+            ]);
+            t.same(metric.meta, {});
+            t.end();
+        });
+
+        gauge.set(1, {
+            labels: {
+                second: 'this is second',
+                third: 'this is third',
+            },
+        });
+    },
+);
